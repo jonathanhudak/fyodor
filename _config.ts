2277d2f134437f6e7e053195a83ea439d467f7d4
Jonathan Hudak <hudak.jonathan@gmail.com>
2023-03-07 21:48:29 -0800

    Add url filter to fix links in gh-pages

--- _config.ts
+++ _config.ts
@@ -1,13 +1,18 @@
 import lume from "lume/mod.ts";
 import netlifyCMS from "lume/plugins/netlify_cms.ts";
 
+const loc = new URL("https://jonathanhudak.github.io/fyodor");
+
 const site: lume.Options = lume({
-  location: new URL("https://jonathanhudak.github.io/fyodor/"),
+  location: loc,
 });
 
 site.copy("media", "media");
 
 site.filter("dateDisplay", (date) => date.toLocaleDateString(), {});
+site.filter("siteUrl", (path) =>
+  Deno.env.get("LOCAL") ? path : new URL(`${loc}${path}`)
+);
 
 site.use(netlifyCMS());
 
